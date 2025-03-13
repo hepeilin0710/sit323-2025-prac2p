3c4a96f8927c8e93584602420f5e7b03087d0a8e
hepeilin0710 <H15539236328@outlook.com>
2025-03-13 15:38:05 +1100

    1

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+// Import the Express.js framework
 const express = require('express');
 const path = require('path');
-
+// Create an Express application
 const app = express();
 const PORT = 3000;
 
-// HTML
+// Serve HTML files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 // start
